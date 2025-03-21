a573c1787fe3e01a4af2f0ae7ea08a0ad470a958
System Administrator <root@infra.local>
2025-03-21 11:30:46 +0530

    version.5

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to today's class! How was it?");
         System.out.println("Welcome to the DevOps learning!");
+	System.out.println("thanks to Vikas Ratnawat Sir!");
         System.out.println("Let's explore automation and CI/CD pipelines.");
     }
 }
