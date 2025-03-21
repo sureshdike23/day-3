2e2feeeea6c590e7868280ef65a52561d86aa0cd
System Administrator <root@infra.local>
2025-03-21 11:27:18 +0530

    my final version.4

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 // HelloWorld.java
-public class HelloWorld Welcome to the today's class How was it? {
+public class HelloWorld { 
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+        System.out.println("Welcome to today's class! How was it?");
+        System.out.println("Welcome to the DevOps learning!");
+        System.out.println("Let's explore automation and CI/CD pipelines.");
     }
 }
 
