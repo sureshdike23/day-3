5b82d73dce1ee0629204a82b8afe8a9c533d1ac5
System Administrator <root@infra.local>
2025-03-21 10:59:46 +0530

    my second change v.2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 // HelloWorld.java
-public class HelloWorld {
+public class HelloWorld Welcome to the today's class {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
     }
